bacddfb4c64176920b5349e2c5d64b4e2c9962c2
Tomiwa Aribisala <tomiwaaribisala@gmail.com>
2023-09-30 22:20:02 +0000

    simple s3 bucket with terrafm

--- main.tf
+++ main.tf
@@ -4,10 +4,18 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.19.0"
+    }
   }
 }
 
 provider "random" {
+  # Configuration options
+}
+
+provider "aws" {
   # Configuration options
 }
 
@@ -16,6 +24,14 @@ resource "random_string" "bucket_name" {
   special          = false
 }
 
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+}
+
 output "random_bucket_name_id" {
   value = random_string.bucket_name.id
+}
+
+output "aws_s3_bucket_example" {
+  value = aws_s3_bucket.example.id
 }
