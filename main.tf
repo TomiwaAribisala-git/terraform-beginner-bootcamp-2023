28c6acc00761ebc996c8fb37a43b610c6cc0f60e
Tomiwa Aribisala <tomiwaaribisala@gmail.com>
2023-10-08 19:03:13 +0000

    terrahouse module

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
+terraform {
+  cloud {
+    organization = "tomiwa-terraform-bootcamp-2023"
+    workspaces {
+      name = "tomiwa-terra-house-1"
+    }
+  }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.19.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
 module "terrahouse_aws" {
   source = "./modules/terrahouse_aws"
 }
--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -1,22 +1,3 @@
-terraform {
-  cloud {
-    organization = "tomiwa-terraform-bootcamp-2023"
-    workspaces {
-      name = "tomiwa-terra-house-1"
-    }
-  }
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.19.0"
-    }
-  }
-}
-
-provider "aws" {
-  # Configuration options
-}
-
 resource "aws_s3_bucket" "test-bucket" {
   bucket = var.bucket_name
 }
