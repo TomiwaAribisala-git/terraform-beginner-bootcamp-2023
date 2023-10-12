674c95cba698f731a15d40d9f5f939f93042e7d1
Tomiwa Aribisala <tomiwaaribisala@gmail.com>
2023-10-12 10:23:25 +0000

    cdn implementation

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "s3_bucket_endpoint" {
   description = "s3 bucket static website endpoint"
   value = module.terrahouse_aws.website_endpoint
 }
+
+output "cdn_domain_name" {
+  description = "cloudfront distribution domain name serving s3 bucket"
+  value = module.terrahouse_aws.cdn_domain_name
+}
