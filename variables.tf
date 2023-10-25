5dd3c422dd5054f32ec19aa54d9676770a37765a
Tomiwa Aribisala <tomiwaaribisala@gmail.com>
2023-10-25 09:05:26 +0000

    assets-upload-and-for-each

--- variables.tf
+++ variables.tf
@@ -7,8 +7,3 @@ variable content_version {
     description = "The version value of the s3 objects"
     type = number
 }
-
-variable assets_path {
-    description = "The path to uploaded assets"
-    type = string
-}
